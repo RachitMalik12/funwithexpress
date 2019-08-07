0e2ad83e1651e698a5f62da7abddeee3e975412f
RachitMalik12 <rachitmalik1947@gmail.com>
2019-08-07 12:23:54 +0200

    Added POST request

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express'); 
 
 const app = express(); 
+// Adding middleware. express.json() returns a middleware and we want to use it. 
+app.use(express.json()); 
 const courses = [
     {id: 1, name: 'cpsc210'},
     {id: 2, name: 'cpsc211'},
@@ -15,6 +17,14 @@ app.get('/api/courses', (req,res) => {
 app.get('/api/courses/:id', (req,res)=> {
     const course = courses.find(c => c.id === parseInt(req.params.id)); 
     if(!course) res.status(404).send('The course with the given id not found'); 
+    res.send(course); 
+})
+app.post('/api/courses', (req,res) => {
+    const course = {
+        id: courses.length + 1, 
+        name: req.body.name 
+    }
+    courses.push(course); 
     res.send(course); 
 })
 //PORT 
